8792f4b7b257f86ae651a8ea182f5035c1007c7a
Amari Bell <bell-amari>
2021-12-29 13:03:32 -0500

    added more js functions to track the incorrect selections

--- assets/logic.js
+++ assets/logic.js
@@ -1,48 +1,97 @@
 // Linked Exsisting HTML with JS
+var section = window.document.querySelector("#quiz_section");
+var startMessage = window.document.querySelector("#start_message");
 var question = window.document.querySelector("#question");
 var list = window.document.querySelector("#list");
 var listItem = window.document.querySelector("#list_item");
 var btn = window.document.querySelector("#clicker");
 var ans_btn = window.document.querySelector("#answer_button");
 var note = window.document.querySelector("#note");
+var time = window.document.querySelector("#time_count");
+var input = window.document.querySelector("#input");
+var saveBtn = window.document.querySelector("#save_btn");
 const wrong = "The answer you have chosen was incorrect";
 const correct = "Great Job! Your answer was correct!"
 var hold_selection = 0;
+var quizNumber = 0;
+var timeCount = 100;
+time.textContent = "Time: " + timeCount;
 
 // Runs when button is clicked
 btn.addEventListener("click", function(){
-    compileQuestion(questions.q1, answersOne.a1, answersOne.a2, answersOne.a3, answersOne.a4, answersOne.hold);
-    console.log("Button Clicked")
+    callQuestions();
+    startMessage.remove();
+    btn.remove();
 }); 
-function Quiz(hold){
-    if(hold === hold_selection) {
+
+function checkAnswers(answer){
+    if (answer === hold_selection) {
         note.textContent = correct;
-        
-        compileQuestion(questions.q2, answersTwo.a1, answersTwo.a2, answersTwo.a3, answersTwo.a4, answersTwo.hold);
     }
-    else note.textContent = wrong;
+    if (answer !== hold_selection){
+        note.textContent = wrong;
+        time.textContent = "Time: " + (timeCount -= 25);
+    }
+        
+}
+
+function callQuestions(){
+    switch(quizNumber){
+        case 0:
+            compileQuestion(questions.q1, answersOne.a1, answersOne.a2, answersOne.a3, answersOne.a4, answersOne.hold);
+            break;
+        case 1:
+            list.replaceChildren();
+            compileQuestion(questions.q2, answersTwo.a1, answersTwo.a2, answersTwo.a3, answersTwo.a4, answersTwo.hold);
+           break;
+        case 2:
+            list.replaceChildren();
+            compileQuestion(questions.q3, answersThree.a1, answersThree.a2, answersThree.a3, answersThree.a4, answersThree.hold);
+            break;
+        case 3:
+            list.replaceChildren();
+            compileQuestion(questions.q4, answersFour.a1, answersFour.a2, answersFour.a3, answersFour.a4, answersFour.hold);
+            break;
+        case 4:
+            question.replaceChildren();
+            list.remove();
+            note.remove();
+            input.hidden = false;
+            saveBtn.hidden = false;
+            break;
+    }
 }
 
 var compileQuestion = function(q, answer1, answer2, answer3, answer4, holdAnswer){
-
     question.textContent = q;
-    ans_btn.textContent = answer1;
-    ans_btn.addEventListener("click", function(){
-        console.log("Answer Button 1 Clicked");
-        hold_selection = 1;
-        Quiz(holdAnswer);
-    }); 
+    var answer = holdAnswer;
 
     var listItemEl = document.createElement("li");
     var liBtn = document.createElement("button");
     listItemEl.className = "";
-    liBtn.textContent = answer2;
+    liBtn.textContent = answer1;
     list.appendChild(listItemEl);
     listItemEl.appendChild(liBtn);
     liBtn.addEventListener("click", function(){
+        console.log("Answer Button 1 Clicked");
+        hold_selection = 1;
+        quizNumber++;
+        checkAnswers(answer);
+        callQuestions();
+    });
+
+    var listItemEl1 = document.createElement("li");
+    var liBtn = document.createElement("button");
+    listItemEl1.className = "";
+    liBtn.textContent = answer2;
+    list.appendChild(listItemEl1);
+    listItemEl1.appendChild(liBtn);
+    liBtn.addEventListener("click", function(){
         console.log("Answer Button 2 Clicked");
         hold_selection = 2;
-        Quiz(holdAnswer);
+        quizNumber++;
+        checkAnswers(answer);
+        callQuestions();
     });
     var listItemEl2 = document.createElement("li");
     var liBtn2 = document.createElement("button");
@@ -53,7 +102,9 @@ var compileQuestion = function(q, answer1, answer2, answer3, answer4, holdAnswer
     liBtn2.addEventListener("click", function(){
         console.log("Answer Button 3 Clicked");
         hold_selection = 3;
-        Quiz(holdAnswer);
+        quizNumber++;
+        checkAnswers(answer);
+        callQuestions();
     });
     var listItemEl3 = document.createElement("li");
     var liBtn3 = document.createElement("button");
@@ -64,45 +115,47 @@ var compileQuestion = function(q, answer1, answer2, answer3, answer4, holdAnswer
     liBtn3.addEventListener("click", function(){
         console.log("Answer Button 4 Clicked");
         hold_selection = 4;
-        Quiz(holdAnswer);
+        quizNumber++;
+        checkAnswers(answer);
+        callQuestions();
     });
 
 }
 
 const questions = {
-    q1 : "What is the value of pi?",
-    q2 : "What type of star is the sun?",
-    q3 : "What is Earth's biggest satillite?",
-    q4 : "How many Space satations are are around earth as of 2021?",
+    q1 : "1 . What is the value of pi?",
+    q2 : "2. What type of star is the sun?",
+    q3 : "3. What is Earth's biggest satillite?",
+    q4 : "4. How many Space satations are are around earth as of 2021?"
 };
 const answersOne = {
-    a1 : "6.14",
-    a2 : "3.54",
-    a3 : "3.14",
-    a4 : "pi",
+    a1 : "A. 6.14",
+    a2 : "B. 3.54",
+    a3 : "C. 3.14",
+    a4 : "D. pi",
     hold: 3
 };
 
 const answersTwo = {
-    a1 : "White Dwarf Star",
-    a2 : "Yellow Darf Star",
-    a3 : "Red Gaint",
-    a4 : "Super Massive Black Hole",
+    a1 : "A. White Dwarf Star",
+    a2 : "B. Yellow Darf Star",
+    a3 : "C. Red Gaint",
+    a4 : "D. Super Massive Black Hole",
     hold: 2
 };
 
 const answersThree = {
-    a1 : "International Space Station",
-    a2 : "Saturn V",
-    a3 : "The Moon",
-    a4 : "Falcon 9",
+    a1 : "A. International Space Station",
+    a2 : "B. Saturn V",
+    a3 : "C. The Moon",
+    a4 : "D. Falcon 9",
     hold: 3
 };
 
 const answersFour = {
-    a1 : "None",
-    a2 : "Twelve",
-    a3 : "Two",
-    a4 : "One",
+    a1 : "A. None",
+    a2 : "B. Twelve",
+    a3 : "C. Two",
+    a4 : "D. One",
     hold: 4
 };
